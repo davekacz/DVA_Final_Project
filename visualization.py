bb0b1c4bc6fa8e848a9b92219084aeac037a3b89
kagarwal <41074929+karn1986@users.noreply.github.com>
2021-11-23 21:14:43 -0700

    fixed bugs
    location = 0 corresponds to no selection
    add markers for current zone and best zones

--- visualization.py
+++ visualization.py
@@ -136,15 +136,14 @@ app.layout = html.Div(children=[
         dcc.Slider(
             id='time_slider_driving_duration',
             min = 1,
-            max = 4,
+            max = 3,
             step = None,            
             marks={
-            1: '15',
-            2: '30',
-            3: '45',
-            4: '60'
+            1: '10',
+            2: '20',
+            3: '30'
             },
-            value= 1 #defaults to the first option
+            value= 2 #defaults to the first option
         ),
         ], 
         style={'width': '15%'}
@@ -158,7 +157,10 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='choropleth',
         figure = {},
-        style={'width': '75%', 'display': 'inline-block'},
+        style={
+            "width": "100%",
+            "height": "100%"
+        },
         config = {'doubleClick': 'reset+autosize',
                   'doubleClickDelay': 600} 
     ),
@@ -211,12 +213,15 @@ def display_selected_data(selectedpoints, relaydata,
         util.duration = time_slider
         year = year_selection
         month = month_selection
-        transition_time = (int(time_slider_driving_duration)*15)/60
+        transition_time = (int(time_slider_driving_duration)*10)/60
         fig = figure
         if selectedpoints is not None:
-            location = selectedpoints['points'][0]['location'] 
+            if ('location' in selectedpoints['points'][0]):
+                location = selectedpoints['points'][0]['location'] 
+            else:
+                location = 0
         else:
-            location = 'null'
+            location = 0
         current_selection = {'dow':day_selection,
                              'day_night': time_selection,
                              'duration':time_slider,
@@ -244,8 +249,8 @@ def display_selected_data(selectedpoints, relaydata,
                 if (current_zoom == home_view) and (last_zoom != home_view):
                     run_callback = True
                     selectedpoints = None
-                    location = 'null'
-                    current_selection['location'] = 'null'
+                    location = 0
+                    current_selection['location'] = 0
                     last_selection = current_selection
                 last_zoom = current_zoom
                     
@@ -258,7 +263,7 @@ def display_selected_data(selectedpoints, relaydata,
         #update utility with relevant information: 
         if run_callback:
         #Checks if there are any selected points, returns zoomed out map if None
-            if location == 'null':
+            if location == 0:
                 print('No Zone Selected')
                 #If no zones are selected, return unzoomed overall map
                 #run pagerank algorithm:
@@ -279,6 +284,8 @@ def display_selected_data(selectedpoints, relaydata,
                 choro_df = pd.merge(choro_df, zone_names, left_on='Zone_ID', right_on='LocationID')
                 choro_df.drop(columns='LocationID', inplace=True)
                 
+                choro_df["Text"] = choro_df.apply(lambda row: "At " + row["borough"] + " borough, " + row["zone"] + " zone," +  " the pickup score is:" + str(round(row["pickup_score"],2)), axis=1)
+                
                 df = choro_df
                 color_data = 'pickup_score'
                 location_ID = "Zone_ID"
@@ -286,12 +293,14 @@ def display_selected_data(selectedpoints, relaydata,
                 center_data = home_view['center']
                 color_scale = "RdBu"
                 zoom_level = home_view['zoom']
-                hover_dataset = ['borough']
+                hover_dataset = {'Text':False, 'Zone_ID':False, 'borough':False,'zone':False, 'pickup_score':False}
                 color_midpoint = np.mean(choro_df['pickup_score'].values)
                 range_color = [choro_df['pickup_score'].min(), 
                                choro_df['pickup_score'].max()]
                 last_zoom = home_view
-    
+                #This is used later in add_scattermapbox, as best_zone is only relevant after a click, we put zero here to avoid getting
+                # an error in add_scattermapbox. I added a 0,0,0 row in centers.csv for this purpose
+                best_zone = 0
             else:
                 print('Calculating top neigbors...')
                 neighbors = util.top_neighbor(location, month, year,
@@ -300,10 +309,18 @@ def display_selected_data(selectedpoints, relaydata,
                 base_amount = neighbors.loc[location, 'expected_total_amount']
                 
                 neighbors['pct_extra'] = (100 * (neighbors['expected_total_amount'] 
-                                                - base_amount)/base_amount)         
+                                                - base_amount)/base_amount)    
+                neighbors[neighbors['pct_extra']<0] = np.nan
                 neighbors = pd.merge(zone_names,neighbors,how = 'left',
                                     left_on='LocationID', right_index = True)
-                
+                #This is constructing a meaningful mouse-hover/over text, it creates a new column in neighbors to store the hover text
+                neighbors["Text"] = neighbors.apply(lambda row: 
+                    "There is no financial benefit in driving to " + row["borough"] + " borough, " + row["zone"] + " zone," +  " compared to your current zone."
+                    if (np.isnan(row["pct_extra"]) or (row["pct_extra"]==0))                   
+                    else
+                        "If you drive to " + row["borough"] + " borough, " + row["zone"] + " zone," +  " the avg trip time for a pickup is: " + str(round(row["avg_trip_time"]*60,2)) 
+                        + " minutes. <br> You will make on avg $" + str(round(row["avg_total_amount"],2)) + " in the time left of your day. This amount is $" + str(round(row["avg_total_amount"]*(row["pct_extra"]/100),2)) + ' more than you current zone.', axis=1)
+ 
                 df = neighbors
                 color_data = 'pct_extra'
                 location_ID = "LocationID"
@@ -312,12 +329,17 @@ def display_selected_data(selectedpoints, relaydata,
                                "lon": centers.loc[location, 'avg_long']}
                 color_scale = "RdBu"
                 zoom_level = 11.5
-                color_midpoint = 0
+                color_midpoint = neighbors['pct_extra'].max()/2
                 range_color = [neighbors['pct_extra'].min(), 
                                neighbors['pct_extra'].max()]
-                hover_dataset = ['borough', 'avg_trip_time', 'expected_total_amount']
+                #This selects which columns you want to show on hover
+                hover_dataset = {'Text':False, 'LocationID':False, 'borough':False,'zone':False, 'avg_trip_time':False, 'avg_total_amount' :False, 'num_trips':False, 'expected_total_amount':False, 'pct_extra':False}
                 last_zoom = {'center': center_data,
                              'zoom': zoom_level}
+                if neighbors['pct_extra'].idxmax():
+                    best_zone = df.loc[df['pct_extra'].idxmax(), 'LocationID']
+                else:
+                    best_zone = 0
                 # neighbors['pct_extra'].fillna(-100, inplace=True)
             fig = px.choropleth_mapbox(
                             df, 
@@ -336,8 +358,36 @@ def display_selected_data(selectedpoints, relaydata,
                             hover_name = 'zone',
                             hover_data=hover_dataset,
                             )
+            
+            fig.add_scattermapbox(lat = [centers.loc[best_zone, 'avg_lat']],
+                    lon = [centers.loc[best_zone, 'avg_long']],
+                    mode = 'markers+text',
+                    text = ['Best Location'],  #a list of strings, one  for each geographical position  (lon, lat)
+                    below='',
+                    marker_size=20, 
+                    marker_color='rgb(0,0,255)',                                                        
+                    textposition = "bottom center", 
+                    textfont=dict(size=20, color='white'),                                                                    
+                    name = 'Best Location',
+                    hoverinfo="none",
+                    # hide from the legend
+                    showlegend=False)
+            
+            fig.add_scattermapbox(lat=[centers.loc[location, 'avg_lat']],
+                                       lon = [centers.loc[location, 'avg_long']],
+                                       mode = 'markers+text',
+                                       text = ['Current Location'],  #a list of strings, one  for each geographical position  (lon, lat)
+                                       below='',
+                                       marker_size=20, 
+                                       marker_color='rgb(170, 255, 0)',
+                                       textposition = "bottom center", 
+                                       textfont=dict(size=20, color='yellow'),
+                                       name = 'Current Location',
+                                       hoverinfo="none",
+                                       # hide from the legend
+                                       showlegend=False )
     
-            fig.update_mapboxes(pitch=45)
+            # fig.update_mapboxes(pitch=45)
             fig.update_layout(clickmode='event+select', 
                               title = 'NYC Cabbie Director', 
                               coloraxis_showscale=True,
